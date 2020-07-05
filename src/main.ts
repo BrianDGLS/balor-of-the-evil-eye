26d2657c3064f40fbd9184102d6426ebb8b49212
BrianDGLS <briandgls92@gmail.com>
2020-07-05 15:14:19 +0100

    feat: added basic game loop

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,42 @@
-console.log('hello world')
+const $canvas = document.createElement('canvas')
+const ctx = $canvas.getContext('2d') as CanvasRenderingContext2D
+
+const width = ($canvas.width = 420)
+const height = ($canvas.height = 420)
+
+document.body.appendChild($canvas)
+
+class Game {
+  public playing = false
+}
+
+class Scheduler {
+  public frame!: number
+
+  private renderFunc!: (timestamp: number) => void
+
+  requestFrame(renderFunc?: (timestamp: number) => void) {
+    if (renderFunc) this.renderFunc = renderFunc
+
+    this.frame = requestAnimationFrame(this.renderFunc)
+  }
+}
+
+const game = new Game()
+const scheduler = new Scheduler()
+
+game.playing = true
+const render = () => {
+  ctx.fillRect(0, 0, width, height)
+
+  if (game.playing) {
+    scheduler.requestFrame()
+  }
+  console.log(scheduler.frame)
+}
+
+scheduler.requestFrame(render)
+
+setTimeout(() => {
+  game.playing = false
+}, 2000)
